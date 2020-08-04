88eef466bf5268673e642d352e8084434045099e
guihehans <guihehans@163.com>
2020-08-04 10:20:07 +0800

    two pointers practice. Now on: triplet_sum_close_to_target.py

--- two_pointers/triplet_sum_close_to_target.py
+++ two_pointers/triplet_sum_close_to_target.py
@@ -25,35 +25,52 @@ Explanation: The triplet [1, 1, 1] has the closest sum to the target.
 """
 
 
-def search_pair(arr, i, target_sum, tmp_sum):
-    pass
-
-def find_closest_sum(closest_sum, target_sum):
-    pass
-
-
 def triplet_sum_close_to_target(arr, target_sum):
+    """
+    again, sort the array first. O(N*logN)
+    loop the array as 1st element of triplet. second as head and tail as end.
+    compare the current sum with target sum. according if the diff is the smaller to change
+    the to return closest sum. if the diff is same , closest sum is smaller one.
+    if sum < taget sum ,the head++
+    if sum > target sum, the end--
+    return the global found closest sum.
+
+    :param arr:
+    :param target_sum:
+    :return:
+    """
     closest_sum = 0
     arr.sort()
     min_diff = 25555
 
     for i in range(len(arr) - 2):
-        cur_num = arr[i]
-        left = arr[i + 1]
-        right = arr[len(arr) - 1]
-        cur_sum = cur_num + left + right
-        diff = abs(target_sum - cur_num)
+        left = i + 1
+        right = len(arr) - 1
 
         while left < right:
+            cur_sum = arr[i] + arr[left] + arr[right]
+            diff = abs(target_sum - cur_sum)
             if diff == 0:
-                return cur_num
+                return cur_sum
             else:
-                min_diff = min(min_diff, diff)
-                closest_sum = cur_num
+                if diff == min_diff:
+                    closest_sum = min(cur_sum, closest_sum)
+                if diff < min_diff:
+                    min_diff = diff
+                    closest_sum = cur_sum
 
                 if cur_sum < target_sum:
                     left += 1
                 elif cur_sum > target_sum:
                     right -= 1
 
-        return closest_sum
+    return closest_sum
+
+
+def main():
+    print(triplet_sum_close_to_target([-2, 0, 1, 2], 2))
+    print(triplet_sum_close_to_target([-3, -1, 1, 2], 1))
+    print(triplet_sum_close_to_target([1, 0, 1, 1], 100))
+
+
+main()
