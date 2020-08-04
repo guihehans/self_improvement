7285ea12545875b09ebc13b94c6d0196d2ecd95d
guihehans <guihehans@163.com>
2020-08-04 18:14:28 +0800

    two pointers practice. Now on: triplet_with_smaller_sum

---
+++ two_pointers/Subarrays with Product Less than a Target.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@author: guihehans
+@file: Subarrays with Product Less than a Target.py
+@time: 2020/8/4 15:58
+@function:
+
+Given an array with positive numbers and a target number,
+find all of its contiguous sub_arrays whose product is less than the target number.
+
+Input: [2, 5, 3, 10], target=30
+Output: [2], [5], [2, 5], [3], [5, 3], [10]
+Explanation: There are six contiguous subarrays whose product is less than the target.
+
+Input: [8, 2, 6, 5], target=50
+Output: [8], [2], [8, 2], [6], [2, 6], [5], [6, 5]
+Explanation: There are seven contiguous subarrays whose product is less than the target.
+
+"""
+
+
+def find_subarrays(arr, target):
+    """
+    need the sub arrays to be CONTIGUOUS. so no sort.
+
+
+    :param arr:
+    :param target:
+    :return:
+    """
+    result = []
+    # TODO: Write your code here
+    return result
