d07bb38cb564aff66c3e5ee39e7ac581b141ae58
guihehans <guihehans@gmail.com>
2020-08-06 00:03:39 +0800

    max_sub_array_of_size_k

--- data_structure/selection_sort.py
+++ data_structure/selection_sort.py
@@ -17,8 +17,12 @@ Space O(1)
 def selection_sort(arr):
     for i in range(len(arr)):
         min_index = i
-        for j in range(i+1,len(arr)):
-            if arr[j]<arr[min_index]:
-                pass
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[min_index]:
+                min_index = j
 
-    pass
+        tmp = arr[i]
+        arr[i] = arr[min_index]
+        arr[min_index] = tmp
+
+    return arr
