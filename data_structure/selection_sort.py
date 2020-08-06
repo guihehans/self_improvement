7b38bcbbcc36cf7780f1a827199d2e9a78f1c3d6
guihehans <guihehans@163.com>
2020-08-06 10:02:24 +0800

    data structure and algorithm: now on: selection_sort.py

--- data_structure/selection_sort.py
+++ data_structure/selection_sort.py
@@ -12,13 +12,37 @@ Time O(n^2)
 Space O(1)
 
 """
+import unittest
 
 
 def selection_sort(arr):
-    for i in range(len(arr)):
+    for i in range(len(arr) - 1):
         min_index = i
-        for j in range(i+1,len(arr)):
-            if arr[j]<arr[min_index]:
-                pass
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        # pythonic way to swap elements. use a ROW_TWO bytecode instruction to swap top 2 elements in stack
+        arr[i], arr[min_index] = arr[min_index], arr[i]
 
-    pass
+    return arr
+
+
+class TestCase(unittest.TestCase):
+    def test(self):
+        arr = [2, 5, 1, 8]
+        target = [1, 2, 5, 8]
+        self.assertTrue(target == selection_sort(arr))
+        nums = [2, 11, 7, 15]
+        target = [2, 7, 11, 15]
+        self.assertTrue(selection_sort(nums) == target)
+        nums = [3, 2, 4]
+        target = [2, 3, 4]
+        self.assertTrue(selection_sort(nums) == target)
+        nums = [4, 3, 2]
+        target = [2, 3, 4]
+        self.assertTrue(selection_sort(nums) == target)
+
+
+
+if __name__ == '__main__':
+    TestCase.test()
