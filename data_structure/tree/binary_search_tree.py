0e09f786a9473639ed7ebb8e92b23b4bf4c705e1
guihehans <guihehans@163.com>
2020-08-14 13:27:11 +0800

    data structure and algorithm: now on: bst find insert

--- data_structure/tree/binary_search_tree.py
+++ data_structure/tree/binary_search_tree.py
@@ -18,6 +18,9 @@ class Node:
         self.left = None
         self.right = None
 
+    def __repr__(self):
+        return "the node value is {}".format(self.val)
+
 
 class BinarySearchTree:
     """
@@ -30,13 +33,60 @@ class BinarySearchTree:
         self.root = None
         if value_list:
             for value in value_list:
-                self.add_node(value)
+                node = Node(value)
+                self.root = self.add_node(self.root, node)
 
-    def add_node(self, value: int):
-        if not self.root:
-            self.root = Node(value)
-
+    def add_node(self, root: Node, new_node: Node):
+        """
+        recursion function to add a new node to a given node.
+        return the modifed root node.
+        :param root:
+        :param new_node:
+        :return: root. return the inserted node.
+        """
+        if root is None:
+            root = new_node
         else:
-            if value < self.root.val:
-                self.add_node()
-        pass
+            if root.val < new_node.val:
+                if root.right is None:
+                    root.right = new_node
+                else:
+                    root.right = self.add_node(root.right, new_node)
+            else:
+                if root.left is None:
+                    root.left = new_node
+                else:
+                    root.left = self.add_node(root.left, new_node)
+        return root
+
+    def in_order(self, root: Node):
+        if root:
+            self.in_order(root.left)
+            print(root.val)
+            self.in_order(root.right)
+
+    def find(self, node: Node, value: int):
+        """
+        find the node which equals the given value.
+        :param node: the given root node to find
+        :param value:
+        :return:
+        """
+        current_node = node
+        while current_node:
+            if current_node.val == value:
+                return current_node
+            else:
+                if current_node.val < value:
+                    return self.find(current_node.right, value)
+                else:
+                    return self.find(current_node.left, value)
+        return "cannot find node in tree"
+
+
+if __name__ == '__main__':
+    arr = [50, 30, 20, 40, 70, 60, 80]
+    bst = BinarySearchTree(arr)
+    bst.in_order(bst.root)
+    node_find=bst.find(bst.root, 40)
+    print(node_find)
