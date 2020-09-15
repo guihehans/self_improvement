fdb42a66a56919e23fc4636d1e12cf811980b6bc
guihehans <guihehans@163.com>
2020-09-15 17:54:57 +0800

    boyer_moore.py only bad character rule

--- data_structure/String/boyer_moore.py
+++ data_structure/String/boyer_moore.py
@@ -7,6 +7,7 @@
 @function:
 
 """
+import sys
 
 
 class BoyerMoore:
@@ -22,4 +23,40 @@ class BoyerMoore:
             self.bc[ord(pattern[j])] = j
 
     def search(self, txt):
-        pass
+        M = self.M
+        N = len(txt)
+        pat = self.pat
+        # bad character rule
+        for i in range(0, N - M):
+            skip = 0
+            for j in range(M - 1, 0, -1):
+                if pat[j] != txt[i + j]:
+                    # the skip move length is the j-(next occur bad char index in pat).
+                    skip = max(1, j - self.bc[ord(txt[i + j])])
+                    break
+            if skip == 0:
+                return i
+
+        return N
+
+
+def main():
+    """Takes a pattern string and an input string as command-line arguments;
+    searches for the pattern string in the text string; and prints the first
+    occurrence of the pattern string in the text string.
+    Will print the pattern after the end of the string if no match is
+    found.
+    """
+    pat = sys.argv[1]
+    txt = sys.argv[2]
+    bm = BoyerMoore(pat)
+    print("text:    {}".format(txt))
+    offset = bm.search(txt)
+    print("pattern:", end=" ")
+    for _ in range(0, offset):
+        print("", end=" ")
+    print(pat)
+
+
+if __name__ == "__main__":
+    main()
