157f6cd7e09b83ed549f51565eabdc24556cdd33
guihehans <guihehans@163.com>
2020-10-22 18:01:51 +0800

    8queen

--- data_structure/backtracking/eight_queue.py
+++ data_structure/backtracking/eight_queue.py
@@ -10,8 +10,8 @@ the eight queues problem is to place 8 queues on 8*8 board, no two queues share 
 """
 
 
-def print_solution(result):
-    N = 8
+def print_solution(result, n):
+    N = n
     for row in range(N):
         for col in range(N):
             if result[row] == col:
@@ -26,6 +26,7 @@ def check_state(row, column, result):
     left_up = column - 1
     right_up = column + 1
 
+    # index from last row(row-1) to 0.
     for r in range(row - 1, -1, -1):
         # result r row,column is occupied, return false
         # result diagonal occupied,return false
@@ -36,15 +37,19 @@ def check_state(row, column, result):
     return True
 
 
-def eight_queue(row, result):
-    if row == 8:
-        print_solution(result)
-    for column in range(0, 8):
+def n_queen(row, result, n):
+    if row == n:
+        print(result)
+        print_solution(result, n)
+    for column in range(n):
         if check_state(row, column, result):
             result[row] = column
-            eight_queue(row + 1, result)
+            n_queen(row + 1, result, n)
 
 
 if __name__ == '__main__':
-    r_list = [0] * 8
-    eight_queue(1, r_list)
+    N = 8
+    r_list = [7, 0, 0, 0, 0, 0, 0, 0]
+    # defalt ,the 0,0 is placed. check from row 1.
+    # so change the first row value we can get all solution.
+    n_queen(1, r_list, N)
