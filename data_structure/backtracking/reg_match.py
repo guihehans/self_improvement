e5a6e762c93d73286fcec199c9ee9d71d254cf77
guihehans <guihehans@163.com>
2020-11-03 18:01:39 +0800

    reg match

---
+++ data_structure/backtracking/reg_match.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@author: guihehans
+@file: reg_match.py
+@time: 2020/11/3 17:44
+@function:
+
+process pattern str and match.
+"*" means 0 or more char.
+"?" means 1 or more char.
+
+
+"""
