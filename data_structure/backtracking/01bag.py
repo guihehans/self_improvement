230ad5e9fc10d635bfb2440b0001dd592a592cb0
guihehans <guihehans@163.com>
2020-11-03 17:18:47 +0800

    01bag.py with each item has price

--- data_structure/backtracking/01bag.py
+++ data_structure/backtracking/01bag.py
@@ -27,6 +27,7 @@ import math
 
 global maxW
 maxW = -math.inf
+maxV = -math.inf
 
 
 def f(i, cw, item_list, n, w):
@@ -51,9 +52,27 @@ def f(i, cw, item_list, n, w):
         f(i + 1, cw + item_list[i], item_list, n, w)
 
 
+def max_weight_with_price(i, cw, cv, item_list, value_list, n, w):
+    if cw == w or i == n:
+        global maxW, maxV
+        if cw > maxW:
+            maxW = cw
+        if cv > maxV:
+            maxV = cv
+        return
+    # check item i not select condition,go to check i+1
+    max_weight_with_price(i + 1, cw, cv, item_list, value_list, n, w)
+    # return from i+1 to i, now select item i
+    if cw + item_list[i] <= w:
+        max_weight_with_price(i + 1, cw + item_list[i], cv + value_list[i], item_list, value_list, n, w)
+
+
 if __name__ == '__main__':
     items = [1, 5, 15, 25, 35, 45, 55, 65, 75, 85]
+    values = [0, 5, 5, 25, 15, 5, 55, 65, 75, 85]
     n = len(items)
     w = 100
-    f(0, 0, items, n, w)
+    # f(0, 0, items, n, w)
+    max_weight_with_price(0, 0, 0, items, values, n, w)
     print(maxW)
+    print(maxV)
