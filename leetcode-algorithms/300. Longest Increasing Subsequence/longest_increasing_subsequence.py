66bbed6a6b2324b99c8769f29da440c2e61f77ed
guihehans <guihehans@163.com>
2020-11-23 17:38:02 +0800

    leetcode 300: longest_increasing_subsequence.py update

--- leetcode-algorithms/300. Longest Increasing Subsequence/longest_increasing_subsequence.py
+++ leetcode-algorithms/300. Longest Increasing Subsequence/longest_increasing_subsequence.py
@@ -11,23 +11,12 @@
 解释: 最长的上升子序列是 [2,3,7,101]，它的长度是 4。
 
 """
+from typing import List
 
 
-class LongestIncreasingSubsequence:
-    def __init__(self, nums):
-        self.nums = nums
-
-    def dp(self):
-        """
-        i 为当前扫描指针， j in [0,i-1]
-        dp[i]记录当前位置处的最长上升子序列长度
-        dp[i]=max(dp[j])+1 for j in [0,i-1] if num[i]>num[j] 否则 跳过
-        每一个dp[i] 都要对j 0:i-1扫描
-        最终最长的dp为 max(dp)
-        :return:
-        """
-        n = len(self.nums)
-        nums = self.nums
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        n = len(nums)
         # init dp[i]=1. every subsequence's lis =1
         dp = [1 for i in range(n)]
         for i in range(1, n):
@@ -38,11 +27,3 @@ class LongestIncreasingSubsequence:
                     continue
 
         return max(dp)
-
-
-def test():
-    nums = [10, 9, 2, 5, 3, 7, 101, 18]
-    lis = LongestIncreasingSubsequence(nums)
-    max_lis = lis.dp()
-    print(max_lis)
-    assert 4 == max_lis
