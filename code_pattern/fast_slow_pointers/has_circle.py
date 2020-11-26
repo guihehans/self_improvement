abe3108753cb92cbd760cb9e39b87b73c42eda49
guihehans <guihehans@163.com>
2020-11-26 14:18:07 +0800

    refactor has_circle.py

---
+++ code_pattern/fast_slow_pointers/has_circle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@author: guihehans
+@file: has_circle.py
+@time: 2020/11/26 10:52
+@function:
+
+"""
+
+
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+
+def has_cycle(head):
+    slow, fast = head, head
+    while fast is not None and fast.next is not None:
+        fast = fast.next.next
+        slow = slow.next
+        if slow == fast:
+            return True
+    return False
+
+
+def main():
+    head = Node(1)
+    head.next = Node(2)
+    head.next.next = Node(3)
+    head.next.next.next = Node(4)
+    head.next.next.next.next = Node(5)
+    head.next.next.next.next.next = Node(6)
+    print("LinkedList has cycle: " + str(has_cycle(head)))
+
+    head.next.next.next.next.next.next = head.next.next
+    print("LinkedList has cycle: " + str(has_cycle(head)))
+
+    head.next.next.next.next.next.next = head.next.next.next
+    print("LinkedList has cycle: " + str(has_cycle(head)))
+
+
+if __name__ == '__main__':
+    main()
