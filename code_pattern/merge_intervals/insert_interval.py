a45b4436b88fd249af04b2c515f27a95c53b50b4
guihehans <guihehans@163.com>
2020-12-02 11:08:12 +0800

    insert interval at correct pos. fast optimized

--- code_pattern/merge_intervals/insert_interval.py
+++ code_pattern/merge_intervals/insert_interval.py
@@ -28,33 +28,69 @@ Explanation: After insertion, since [1,4] overlaps with [2,3], we merged them in
 """
 
 
-def insert(intervals, new_interval):
+def insert_my(intervals, new_interval):
     if len(intervals) < 1:
         return [new_interval]
 
     merged = []
     # 1. insert new_interval at correct index
     # 2. iterating the list, merge the overlapped intervals.
-    insert_index = 0
     for i in range(len(intervals)):
-        a = intervals[i]
-        b = new_interval
-        if b[1] <= a[1]:
-            insert_index = 0
-            break
-        elif a[1] <= b[0]:
-            insert_index = i
-            break
-        elif True:
-            break
+        if new_interval[0] <= intervals[i][0]:
+            intervals.insert(i, new_interval)
+        elif i == len(intervals) - 1:
+            intervals.append(new_interval)
+
+    start, end = intervals[0][0], intervals[0][1]
+    for i in range(1, len(intervals)):
+        next_start, next_end = intervals[i][0], intervals[i][1]
+        if next_start <= end:  # overlap, merge end
+            end = max(end, next_end)
+        else:  # no overlap, push current [start,end] and make interval a=b. loop to compare with next interval
+            merged.append([start, end])
+            start = next_start
+            end = next_end
+    merged.append([start, end])
 
     return merged
 
 
-def main():
+def insert(intervals, new_interval):
+    if len(intervals) < 1:
+        return [new_interval]
+
+    merged = []
+    n = len(intervals)  # use n to avoid counting cost
+    # One loop idea.
+    # 1. push all intervals a that ends before b start
+    i = 0
+    while i < n and intervals[i][1] < new_interval[0]:
+        merged.append(intervals[i])
+        i += 1
+    # 2. now merge the intervals that overlap with new_interval b
+    # overlaps condition: a.start<=b.end
+    #    the new_interval start =min(a_start,b_start) end=max(a_end,b_end)
+    #   continue merge all a and b until no overlap
+    while i < n and intervals[i][0] <= new_interval[1]:
+        new_interval[0] = min(intervals[i][0], new_interval[0])
+        new_interval[1] = max(intervals[i][1], new_interval[1])
+        i += 1
+    # push the final merged interval
+    merged.append(new_interval)
+
+    # 3. append rest non overlap interval a that a.start> b.ends
+    while i < n and intervals[i][0] > new_interval[1]:
+        merged.append(intervals[i])
+        i += 1
+
+    return merged
+
+
+def test():
     print("Intervals after inserting the new interval: " + str(insert([[1, 3], [5, 7], [8, 12]], [4, 6])))
     print("Intervals after inserting the new interval: " + str(insert([[1, 3], [5, 7], [8, 12]], [4, 10])))
     print("Intervals after inserting the new interval: " + str(insert([[2, 3], [5, 7]], [1, 4])))
 
 
-main()
+if __name__ == '__main__':
+    test()
