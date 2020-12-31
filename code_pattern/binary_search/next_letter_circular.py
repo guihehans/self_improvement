a658bda4a60247fbc0bb6ea4e44cb0ed387d548e
guihehans <guihehans@163.com>
2020-12-31 10:30:37 +0800

    my version and simpler version

--- code_pattern/binary_search/next_letter_circular.py
+++ code_pattern/binary_search/next_letter_circular.py
@@ -10,7 +10,16 @@
 import bisect
 
 
-def search_next_letter(letters, key):
+def search_next_letter_my(letters, key):
+    """
+    my solution.
+    split the part to. key<mid key> mid key==mid.
+    when key =mid, check the mid+1.
+
+    :param letters:
+    :param key:
+    :return:
+    """
     n = len(letters)
     start, end = 0, n - 1
     if key > letters[end] or key < letters[0]:
@@ -34,6 +43,22 @@ def search_next_letter(letters, key):
     return letters[start]
 
 
+def search_next_letter(letters, key):
+    n = len(letters)
+    start, end = 0, n - 1
+
+    while start <= end:
+        mid = start + ((end - start) >> 1)
+        if key < letters[mid]:
+            end = mid - 1
+        else: # key>= letters[mid]
+            start = mid + 1
+    # when loop ends, start=end+1, and start is next big element of key.
+    # if start>=n, mod n to make it in circular.
+
+    return letters[start % n]
+
+
 def test():
     assert (search_next_letter(['a', 'c', 'f', 'h'], 'f')) == 'h'
     assert (search_next_letter(['a', 'c', 'f', 'h'], 'b')) == 'c'
@@ -42,6 +67,6 @@ def test():
     assert (search_next_letter(["e", "e", "e", "e", "e", "e", "n", "n", "n", "n"], 'e')) == 'n'
     assert (search_next_letter(["z", "a", "b"], 'z')) == 'z'
 
-    index = bisect.bisect(letters, target)
+
 if __name__ == '__main__':
     test()
