871fe714e2f60185379865c386440ded26bdef68
guihehans <guihehans@163.com>
2021-01-08 14:34:59 +0800

    slide window version

--- code_pattern/top_k/k_closest_element.py
+++ code_pattern/top_k/k_closest_element.py
@@ -12,7 +12,7 @@ from heapq import *
 from collections import deque
 
 
-def find_closest_elements(arr, K, X):
+def find_closest_elements_heap(arr, K, X):
     n = len(arr)
 
     idx = binary_search(arr, X)
@@ -33,7 +33,7 @@ def find_closest_elements(arr, K, X):
 def binary_search(arr, key):
     n = len(arr)
     start, end = 0, n - 1
-    while start < end:
+    while start <= end:
         mid = start + ((end - start) >> 1)
         if key == arr[mid]:
             return mid
@@ -50,6 +50,55 @@ def binary_search(arr, key):
 
 def get_dist(X, Y):
     return abs(Y - X)
+
+
+def find_closest_elements(arr, K, X):
+    n = len(arr)
+
+    idx = binary_search(arr, X)
+    start, end = idx, idx + 1
+
+    result = deque()
+    for i in range(K):
+        if start >= 0 and end <= n - 1:
+            if X - arr[start] <= arr[end] - arr[idx]:
+                result.appendleft(arr[start])
+                start -= 1
+            else:
+                result.append(arr[end])
+                end += 1
+        elif start >= 0:  # end reach n-1
+            result.appendleft(arr[start])
+            start -= 1
+        elif end <= n - 1:  # start reach 0
+            result.append(arr[end])
+            end += 1
+    return list(result)
+
+
+def find_closest_elements_x(arr, K, X):
+    result = deque()
+    index = binary_search(arr, X)
+    leftPointer, rightPointer = index, index + 1
+    n = len(arr)
+    for i in range(K):
+        if leftPointer >= 0 and rightPointer < n:
+            diff1 = abs(X - arr[leftPointer])
+            diff2 = abs(X - arr[rightPointer])
+            if diff1 <= diff2:
+                result.appendleft(arr[leftPointer])
+                leftPointer -= 1
+            else:
+                result.append(arr[rightPointer])
+                rightPointer += 1
+        elif leftPointer >= 0:
+            result.appendleft(arr[leftPointer])
+            leftPointer -= 1
+        elif rightPointer < n:
+            result.append(arr[rightPointer])
+            rightPointer += 1
+
+    return result
 
 
 def test():
@@ -82,6 +131,25 @@ def test_3():
     x = 3
     expected = [1, 2, 3, 4]
     assert set(find_closest_elements(arr, k, x)) == set(expected)
+
+
+def test_4():
+    arr = [1, 1, 1, 10, 10, 10]
+    k = 1
+    x = 9
+    expected = [10]
+    assert set(find_closest_elements(arr, k, x)) == set(expected)
+
+
+def test_5():
+    arr = [0, 1, 2, 3, 4, 4, 4, 5, 5, 5, 6, 7, 9, 9, 10, 10, 11, 11, 12, 13, 14, 14, 15, 17, 19, 19, 22, 24, 24, 25, 25,
+           27, 27, 29, 30, 32, 32, 33, 33, 35, 36, 38, 39, 41, 42, 43, 44, 44, 46, 47, 48, 49, 52, 53, 53, 54, 54, 57,
+           57, 58, 59, 59, 59, 60, 60, 60, 61, 61, 62, 64, 66, 68, 68, 70, 72, 72, 74, 74, 74, 75, 76, 76, 77, 77, 80,
+           80, 82, 83, 85, 86, 87, 87, 92, 93, 94, 96, 96, 97, 98, 99]
+    k = 25
+    x = 90
+    expected = [72, 74, 74, 74, 75, 76, 76, 77, 77, 80, 80, 82, 83, 85, 86, 87, 87, 92, 93, 94, 96, 96, 97, 98, 99]
+    assert (find_closest_elements(arr, k, x)) == (expected)
 
 
 if __name__ == '__main__':
