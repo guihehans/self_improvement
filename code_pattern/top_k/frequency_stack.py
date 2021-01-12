0c8a0ddca470541926e274bd0eb74afc2bc7c7a6
guihehans <guihehans@163.com>
2021-01-12 16:05:35 +0800

    test passed

--- code_pattern/top_k/frequency_stack.py
+++ code_pattern/top_k/frequency_stack.py
@@ -7,7 +7,7 @@
 @function:
 
 """
-
+import collections
 from heapq import *
 import heapq
 
@@ -15,36 +15,61 @@ import heapq
 class FreqStack:
 
     def __init__(self):
-        self.stack = []
-        self.max_heap = []
-        self.index = {}
-        self.n = 0
+        # Counter encapsulate the freq counter function. provide key not exit, it's default value to 0.
+        self.freq = collections.Counter()
+        # defaultdict accept a construct function(list). if key not exist, create a list as stack
+        self.group = collections.defaultdict(list)
+        self.max_freq = 0
 
     def push(self, x: int) -> None:
-        self.stack.append(x)
-        if x not in self.index:
-            self.index[x] = self.n
-            heappush(self.max_heap, (-1, x))
-            self.n += 1
-        else:
-            pos = self.index[x]
-            freq, item = self.max_heap[pos]
-            self.max_heap[pos] = (freq - 1, item)
-            heapify(self.max_heap)
-            self.index[x] = search_heap(self.max_heap, x)
+        f = self.freq[x] + 1
+        self.freq[x] = f
+        if f > self.max_freq:
+            self.max_freq = f
+        self.group[f].append(x)
 
     def pop(self) -> int:
-        pass
+        x = self.group[self.max_freq].pop()
+        self.freq[x] -= 1
+        # if group [max_freq] is empty, then the max_freq no element, max_freq--.
+        if not self.group[self.max_freq]:
+            self.max_freq -= 1
+        return x
 
-
-def search_heap(max_heap, x):
-    for i in range(len(max_heap)):
-        freq, item = max_heap[i]
-        if x == item:
-            return i
-    return -1
 
 # Your FreqStack object will be instantiated and called as such:
 # obj = FreqStack()
 # obj.push(x)
 # param_2 = obj.pop()
+
+
+def test():
+    frequencyStack = FreqStack()
+    frequencyStack.push(1)
+    frequencyStack.push(2)
+    frequencyStack.push(3)
+    frequencyStack.push(2)
+    frequencyStack.push(1)
+    frequencyStack.push(2)
+    frequencyStack.push(5)
+    print(frequencyStack.pop())
+    print(frequencyStack.pop())
+    print(frequencyStack.pop())
+
+
+def test_1():
+    frequencyStack = FreqStack()
+    frequencyStack.push(5)
+    frequencyStack.push(7)
+    frequencyStack.push(5)
+    frequencyStack.push(7)
+    frequencyStack.push(4)
+    frequencyStack.push(5)
+    print(frequencyStack.pop())
+    print(frequencyStack.pop())
+    print(frequencyStack.pop())
+    print(frequencyStack.pop())
+
+
+if __name__ == '__main__':
+    test()
