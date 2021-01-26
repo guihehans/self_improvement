438dc2c694fc71917b292735ac71e20bb0c2ff2c
guihehans <guihehans@163.com>
2021-01-26 16:23:56 +0800

    add hashtable and slide window solution

--- code_pattern/k_way_merge/smallest_range_in_M_sorted_lists.py
+++ code_pattern/k_way_merge/smallest_range_in_M_sorted_lists.py
@@ -7,11 +7,12 @@
 @function:
 
 """
+import collections
 import math
 from heapq import *
 
 
-def find_smallest_range(lists):
+def find_smallest_range_my(lists):
     min_heap = []
     range_start, range_end = 0, math.inf
     cur_max = -math.inf
@@ -32,6 +33,43 @@ def find_smallest_range(lists):
     return [range_start, range_end]
 
 
+def find_smallest_range(nums):
+    n = len(nums)
+    cur_min, cur_max = math.inf, -math.inf
+
+    indices = collections.defaultdict(list)
+
+    for i, arr in enumerate(nums):
+        for x in arr:
+            indices[x].append(i)
+            cur_max = max(cur_max, *arr)
+            cur_min = min(cur_min, *arr)
+
+    freq = [0] * n
+    left, right = cur_min, cur_min - 1
+    best_left, best_right = cur_min, cur_max
+    inside = 0
+
+    while right < cur_max:
+        right += 1
+        if right in indices:
+            for x in indices[right]:
+                freq[x] += 1
+                if freq[x] == 1:
+                    inside += 1
+        while inside == n:
+            if right - left < best_right - best_left:
+                best_left, best_right = left, right
+            if left in indices:
+                for x in indices[left]:
+                    freq[x] -= 1
+                    if freq[x] == 0:
+                        inside -= 1
+            left += 1
+
+    return [best_left, best_right]
+
+
 def test():
     lists = [[1, 5, 8], [4, 12], [7, 8, 10]]
     expected = [4, 7]
@@ -47,6 +85,12 @@ def test_1():
 def test_2():
     lists = [[1, 2, 3], [1, 2, 3], [1, 2, 3]]
     expected = [1, 1]
+    assert find_smallest_range(lists) == expected
+
+
+def test_3():
+    lists = [[-1, 2, 3], [1], [1, 2], [1, 1, 3]]
+    expected = [1, 2]
     assert find_smallest_range(lists) == expected
 
 
