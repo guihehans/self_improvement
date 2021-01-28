e0e4b8f344302cdf593aa926d88b8f3fe99dfa29
guihehans <guihehans@163.com>
2021-01-28 16:59:50 +0800

    add backtracking method

--- code_pattern/01knapsack/01knapsack.py
+++ code_pattern/01knapsack/01knapsack.py
@@ -10,7 +10,7 @@
 from typing import List
 
 
-def solve_knapsack_dp(profits: List[int], weights: List[int], capacity: int):
+def solve_knapsack_dp(profits: List[int], weights: List[int], capacity: int) -> int:
     states = [-1 for _ in range(capacity + 1)]
     n = len(weights)
     assert n == len(profits)
@@ -32,11 +32,38 @@ def solve_knapsack_dp(profits: List[int], weights: List[int], capacity: int):
     return max(states)
 
 
-def test():
-    print(solve_knapsack([1, 6, 10, 16], [1, 2, 3, 5], 5))
-    print(solve_knapsack([1, 6, 10, 16], [1, 2, 3, 5], 6))
-    print(solve_knapsack([1, 6, 10, 16], [1, 2, 3, 5], 7))
+class Knapsack:
 
+    def __init__(self, profits=[], weights=[], capacity=0):
+        self.capacity = capacity
+        self.weights = weights
+        self.profits = profits
+        self.n = len(profits)
+        self.max_profit = 0
+
+    def solve_knapsack(self, profits: List[int], weights: List[int], capacity: int) -> int:
+        self.__init__(profits, weights, capacity)
+        self.backtracking(0, 0, 0)  # invoke from item0, current_weight=0, and current_profit=0
+        return self.max_profit
+
+    def backtracking(self, index: int, cur_weight, cur_profit):
+        if cur_weight == self.capacity or index == self.n:
+            if cur_profit > self.max_profit:  # the max_profit update only when reach bottom.
+                self.max_profit = cur_profit
+            return
+
+        self.backtracking(index + 1, cur_weight, cur_profit)
+        if cur_weight + self.weights[index] <= self.capacity:  # 剪枝 pruning
+            self.backtracking(index + 1, cur_weight + self.weights[index], cur_profit + self.profits[index])
+
+
+def test():
+    k = Knapsack()
+    print(k.solve_knapsack([1, 6, 10, 16], [1, 2, 3, 5], 5))
+    print(k.solve_knapsack([1, 6, 10, 16], [1, 2, 3, 5], 6))
+    print(k.solve_knapsack([1, 6, 10, 16], [1, 2, 3, 5], 7))
+    print(k.solve_knapsack([3, 4, 8, 9,6], [2, 2, 4, 6,3], 9))
+    print(solve_knapsack_dp([3, 4, 8, 9,6], [2, 2, 4, 6,3], 9))
 
 if __name__ == '__main__':
     test()
